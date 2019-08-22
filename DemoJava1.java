4bd4307527f8193a68bafea3713c6cc4cc684023
pawan081296 <pawanch08@gmail.com>
2019-08-22 15:41:23 +0530

    Java Files added

---
+++ DemoJava1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class DemoJava1
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<10;i++){
+		System.out.println("Hello world and First git and jenkins integrartion............."+i);}
+	}
+}
